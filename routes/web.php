9b5f3391044bde8c56cfc2a43728f83064d6e325
David Cabrera <cabrerabuelvasd@gmail.com>
2024-10-24 16:21:56 -0500

    Cambio

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\UserController;
 
 
 
-Route::resource('users', UserController::class, true);
+Route::resource('users', UserController::class);
 
-Route::post('/users/{id}/enable', [UserController::class, 'enable'])->name('users.enable', true);
-Route::post('/users/{id}/disable', [UserController::class, 'disable'])->name('users.disable', true);
+Route::post('/users/{id}/enable', [UserController::class, 'enable'])->name('users.enable');
+Route::post('/users/{id}/disable', [UserController::class, 'disable'])->name('users.disable');
 
 Auth::routes();
 
