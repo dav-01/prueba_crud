4a20fe7b6bbfffeaa59bfbe4444fd5a24aca2217
David Cabrera <cabrerabuelvasd@gmail.com>
2024-10-24 15:28:05 -0500

    Cambio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Administración de Usuarios')</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BuZUYtlX.css') }}">
-    <script src="{{ asset('build/assets/app-fGIFnpOl.js') }}" defer></script>
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 min-h-screen">
 
